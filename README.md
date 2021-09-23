4ebf178d3048e7180469f99f18a5a09459a66f33
George Pollard <porges@porg.es>
2021-09-23 15:44:31 +1200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,127 @@
+# snowpack-plugin-image-resizer
+
+A [snowpack](https://github.com/snowpackjs/snowpack#readme) plugin for loading images and resizing them to use as responsive images.
+
+## ⚠️ Experimental ⚠️
+
+At the moment I have only used this with [Astro](https://github.com/snowpackjs/astro#readme). It is somewhat inspired by [responsive-loader](https://github.com/dazuaz/responsive-loader#readme), which I used to use with `webpack`.
+
+## Usage
+
+### Installation
+
+First, install the package:
+
+```sh
+npm install --save-dev snowpack-plugin-image-resizer
+```
+
+Then, add the plugin to your project’s config file:
+
+#### `snowpack.config.js`
+```js
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  plugins: [
+    ["snowpack-plugin-image-resizer"]
+  ],
+};
+```
+
+Done! Now, any image imports (with the configured extensions) will be processed by the plugin and multiple outputs in different sizes will be generated.
+
+The result of importing an image is a JavaScript structure with type `ImageImport`:
+
+```ts
+type ImageImport = {
+    /** The original, unmodified, image URI. */
+    src: string;
+    /** The width of the original image. */
+    width: number;
+    /** The height of the original image. */
+    height: number;
+    /** The resized images in srcset format (for use on `<img>`).
+     * Note that to use the `srcset` you will also need to provide your
+     * own `sizes` attribute, which this library cannot create for you.
+     * Refer to MDN’s Reponsive Images article for more. */
+    srcset: string;
+    /** The resized images in a structured format, in case
+     * you need to do anything else with them. */
+    images: {
+        src: string;
+        width: number;
+    }[];
+};
+```
+
+This means you can use the imported image as follows:
+
+```ts
+import myImage from "./myImage.png";
+…
+return (
+  <img
+   src={myImage.src}
+   width={myImage.width}
+   height={myImage.height}
+   srcset={myImage.srcset} 
+   alt="…" />
+);
+```
+
+### TypeScript Support
+
+If you are using TypeScript, you can add support for these image imports by adding a file like this to your project:
+
+#### `images.d.ts`
+```ts
+declare module "*.png" {
+    import { ImageImport } from "snowpack-plugin-image-resizer";
+    const value: ImageImport;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    import { ImageImport } from "snowpack-plugin-image-resizer";
+    const value: ImageImport;
+    export default value;
+}
+
+declare module "*.jpg" {
+    import { ImageImport } from "snowpack-plugin-image-resizer";
+    const value: ImageImport;
+    export default value;
+}
+
+declare module "*.webp" {
+    import { ImageImport } from "snowpack-plugin-image-resizer";
+    const value: ImageImport;
+    export default value;
+}
+```
+
+## Configuring
+
+You can also set some options. Their defaults are shown below:
+
+#### `snowpack.config.js`
+```js
+/** @type {import("snowpack-plugin-image-resizer").Options} */
+const imageLoaderOptions = {
+  // file extensions to load
+  inputs: [".jpg", ".jpeg", ".png", ".webp"],
+
+  // target sizes to resize to
+  sizes: [300, 600, 800, 1200, 1600],
+
+  // images smaller than this are turned into data: uris
+  inlineSizeLimit: 1024,
+};
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  plugins: [
+    ["snowpack-plugin-image-resizer", imageLoaderOptions]
+  ],
+};
+```
---
+++ src/index.ts
@@ -0,0 +1,125 @@
+import { SnowpackPluginFactory, SnowpackConfig, SnowpackPlugin, PluginLoadResult } from 'snowpack';
+
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import * as sharp from 'sharp';
+import * as mime from 'mime';
+
+const defaultExtensions = [".jpg", ".jpeg", ".png", ".webp"];
+const defaultSizes = [300, 600, 800, 1200, 1600];
+const defaultInlineSizeLimit = 1024;
+
+export type Options = {
+  inputs?: string[],
+  sizes?: number[],
+  inlineSizeLimit?: number,
+}
+
+export type ImageImport = {
+  /** The original, unmodified, image URI. */
+  src: string,
+  /** The width of the original image. */
+  width: number,
+  /** The height of the original image. */
+  height: number,
+
+  /** The resized images in srcset format (for use on `<img>`).
+   * Note that to use the `srcset` you will also need to provide your 
+   * own `sizes` attribute, which this library cannot create for you. 
+   * Refer to MDN’s [Reponsive Images](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images)
+   * article for more. */
+  srcset: string,
+
+  /** The resized images in a structured format, in case
+   * you need to do anything else with them. */
+  images: { src: string, width: number }[],
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _checkType: SnowpackPluginFactory<Options> = plugin;
+
+export default function plugin(snowpackConfig: SnowpackConfig, options?: Options): SnowpackPlugin {
+
+  const inlineSizeLimit = options?.inlineSizeLimit || defaultInlineSizeLimit;
+  const input = options?.inputs || defaultExtensions;
+  const sizes = options?.sizes || defaultSizes;
+  const output = [".js", ...input.flatMap(ext => sizes.map(s => `.${s}${ext.replace('.', '-')}`)), ...input];
+
+  return {
+    name: 'my-snowpack-plugin',
+    resolve: { input, output },
+    async load(obj): Promise<PluginLoadResult> {
+      const { filePath, fileExt } = obj;
+
+      const data = await fs.readFile(filePath, 'binary');
+      const rawData = Buffer.from(data, 'binary');
+
+      if (rawData.length <= inlineSizeLimit) {
+        const encoded = rawData.toString('base64');
+        const mimeType = mime.getType(fileExt);
+        return {
+          [".js"]: {
+            code: `export default 'data:${mimeType};base64,${encoded}'`
+          },
+        }
+      } else {
+        const img = sharp(rawData).rotate(/* handle EXIF orientation */);
+        const { width, height } = await img.metadata();
+
+        if (!width || !height) {
+          throw new Error(`Image metadata not found for ${filePath}`);
+        }
+
+        const url = toUrl(snowpackConfig, filePath);
+
+        const goodSizes = sizes.filter(s => s < width);
+
+        const resized = await Promise.all(goodSizes.map(async size => ({
+          width: size,
+          src: `${url}.${size}${fileExt.replace('.', '-')}`,
+          data:
+            await img.clone()
+              .resize(size, null)
+              .jpeg({ mozjpeg: true })
+              .toBuffer(),
+        })));
+
+        // include unresized version
+        resized.push({
+          width: width,
+          src: url + fileExt, // file extension will be duplicated
+          data: rawData,
+        });
+
+        const jsExport: ImageImport = {
+          src: url + fileExt,
+          width: width,
+          height: height,
+
+          srcset: resized.map(i => `${i.src} ${i.width}w`).join(', '),
+          images: resized.map(i => ({ width: i.width, src: i.src })),
+        }
+
+        const result: PluginLoadResult = {
+          [".js"]: { code: `export default ${JSON.stringify(jsExport)};` },
+        };
+
+        for (const img of resized) {
+          result[path.extname(img.src)] = { code: img.data };
+        }
+
+        return result;
+      }
+    },
+  }
+}
+
+function toUrl(cfg: SnowpackConfig, filePath: string): string {
+  for (const [mount, mapping] of Object.entries(cfg.mount)) {
+    if (filePath.startsWith(mount)) {
+      return filePath.replace(mount, mapping.url).replace(/\\/g, '/');
+    }
+  }
+
+  return filePath;
+}
